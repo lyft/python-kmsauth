5cb7ca9c733c9c32e0ed4aa49f35857730a475a2
Chris Steipp <csteipp@lyft.com>
2016-08-30 14:37:34 -0700

    Also bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 
 from setuptools import setup, find_packages
 
-VERSION = "0.1.6"
+VERSION = "0.1.7"
 
 requirements = [
     # Boto3 is the Amazon Web Services (AWS) Software Development Kit (SDK)
