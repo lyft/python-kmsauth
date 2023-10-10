ba4213bc3faa132e075185300b0be437d71fc348
Ramon Petgrave <32398091+ramonpetgrave64@users.noreply.github.com>
2023-10-10 09:35:40 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 
 from setuptools import setup, find_packages
 
-VERSION = "0.6.2.dev3"
+VERSION = "0.6.2.dev1"
 
 requirements = [
     # Boto3 is the Amazon Web Services (AWS) Software Development Kit (SDK)
